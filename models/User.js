40566fa9046f7ee8c856cb03827e355fb4f216b9
wisestonster <wisestonster@gmail.com>
2020-12-14 16:22:34 +0900

    게시판 - User Error 처리

--- models/User.js
+++ models/User.js
@@ -1,18 +1,38 @@
 // models/User.js
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
-// schema // 1
+// schema
 var userSchema = mongoose.Schema({
-  username:{type:String, required:[true,'Username is required!'], unique:true},
-  password:{type:String, required:[true,'Password is required!'], select:false},
-  name:{type:String, required:[true,'Name is required!']},
-  email:{type:String}
+  username:{
+    type:String,
+    required:[true,'Username is required!'],
+    match:[/^.{4,12}$/,'Should be 4-12 characters!'],
+    trim:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:[true,'Password is required!'],
+    select:false
+  },
+  name:{
+    type:String,
+    required:[true,'Name is required!'],
+    match:[/^.{4,12}$/,'Should be 4-12 characters!'],
+    trim:true
+  },
+  email:{
+    type:String,
+    match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'Should be a vaild email address!'],
+    trim:true
+  }
 },{
   toObject:{virtuals:true}
 });
 
-// virtuals // 2
+// virtuals 
 userSchema.virtual('passwordConfirmation')
   .get(function(){ return this._passwordConfirmation; })
   .set(function(value){ this._passwordConfirmation=value; });
@@ -30,34 +50,61 @@ userSchema.virtual('newPassword')
   .set(function(value){ this._newPassword=value; });
 
 // password validation // 3
+// password validation // 2
+var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+var passwordRegexErrorMessage = 'Should be minimum 8 characters of alphabet and number combination!';
 userSchema.path('password').validate(function(v) {
-  var user = this; // 3-1
+  var user = this;
 
-  // create user // 3-3
-  if(user.isNew){ // 3-2
+  // create user
+  if(user.isNew){
     if(!user.passwordConfirmation){
       user.invalidate('passwordConfirmation', 'Password Confirmation is required.');
     }
 
-    if(user.password !== user.passwordConfirmation) {
+    if(!passwordRegex.test(user.password)){
+      user.invalidate('password', passwordRegexErrorMessage);
+    }
+    else if(user.password !== user.passwordConfirmation) {
       user.invalidate('passwordConfirmation', 'Password Confirmation does not matched!');
     }
   }
 
-  // update user // 3-4
+  // update user
   if(!user.isNew){
     if(!user.currentPassword){
       user.invalidate('currentPassword', 'Current Password is required!');
     }
-    else if(user.currentPassword != user.originalPassword){
+    else if(!bcrypt.compareSync(user.currentPassword, user.originalPassword)){
       user.invalidate('currentPassword', 'Current Password is invalid!');
     }
 
-    if(user.newPassword !== user.passwordConfirmation) {
+    if(user.newPassword && !passwordRegex.test(user.newPassword)){
+      user.invalidate("newPassword", passwordRegexErrorMessage);
+    }
+    else if(user.newPassword !== user.passwordConfirmation) {
       user.invalidate('passwordConfirmation', 'Password Confirmation does not matched!');
     }
   }
 });
+
+// hash password // 3
+userSchema.pre('save', function (next){
+  var user = this;
+  if(!user.isModified('password')){ // 3-1
+    return next();
+  }
+  else {
+    user.password = bcrypt.hashSync(user.password); //3-2
+    return next();
+  }
+});
+
+// model methods // 4
+userSchema.methods.authenticate = function (password) {
+  var user = this;
+  return bcrypt.compareSync(password,user.password);
+};
 
 // model & export
 var User = mongoose.model('user',userSchema);
